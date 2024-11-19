e1086a957459639e870d7b3229b04752df807e4c
jedrzejwalega <jedrzejwalega@gmail.com>
2024-11-19 16:10:01 +0000

    Define another model (ViT) to use wih LoRA for fine-tuning on synapse formation dataset

---
+++ 6 - Appsilon/utils/vit_lora.py
@@ -0,0 +1,70 @@
+import torch.nn as nn
+import torch.optim as optim
+from lightning import LightningModule
+from timm import create_model
+from torchmetrics import Accuracy, F1Score
+from torch.optim.lr_scheduler import OneCycleLR
+from peft import LoraConfig, get_peft_model
+
+class VitModel(LightningModule):
+    def __init__(self, num_classes, in_chans, steps_per_epoch, learning_rate, max_epochs, 
+                 lora_r_alpha, lora_target_modules, lora_dropout, lora_bias):
+        super().__init__()
+        non_lora_model = create_model(
+            'vit_base_patch16_224',
+            pretrained=True,
+            num_classes=num_classes,
+            in_chans=in_chans
+        )
+
+        lora_config = LoraConfig(
+            r=lora_r_alpha,
+            lora_alpha=lora_r_alpha,
+            target_modules=lora_target_modules,
+            lora_dropout=lora_dropout,
+            bias=lora_bias
+        )
+
+        self.model = get_peft_model(non_lora_model, lora_config)
+        self.learning_rate = learning_rate
+        self.max_epochs = max_epochs
+        self.steps_per_epoch = steps_per_epoch
+
+        self.train_acc = Accuracy(num_classes=num_classes, task="multiclass")
+        self.val_acc = Accuracy(num_classes=num_classes, task="multiclass")
+        self.f1_score = F1Score(num_classes=num_classes, task="multiclass", average="macro")
+
+    def forward(self, x):
+        return self.model(x)
+
+    def training_step(self, batch, batch_idx):
+        inputs, labels = batch
+        outputs = self(inputs)
+        loss = nn.CrossEntropyLoss()(outputs, labels)
+        self.log("train_loss", loss)
+        self.log("train_acc", self.train_acc(outputs, labels), prog_bar=True)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        inputs, labels = batch
+        outputs = self(inputs)
+        loss = nn.CrossEntropyLoss()(outputs, labels)
+        self.log("val_loss", loss, prog_bar=True)
+        self.log("val_acc", self.val_acc(outputs, labels), prog_bar=True)
+        self.log("val_f1", self.f1_score(outputs, labels), prog_bar=True)
+        return loss
+
+    def configure_optimizers(self):
+        optimizer = optim.Adam(self.parameters(), lr=self.learning_rate)
+        self.logger.experiment["optimizer/lr"].log(self.learning_rate)
+        optimizer_name = optimizer.__class__.__name__
+        self.logger.experiment["optimizer/name"].log(optimizer_name)
+
+        scheduler = OneCycleLR(
+            optimizer,
+            max_lr=self.learning_rate,
+            epochs=self.max_epochs,
+            steps_per_epoch=self.steps_per_epoch
+        )
+        
+        return {"optimizer": optimizer, "lr_scheduler": scheduler, "monitor": "val_f1"}
